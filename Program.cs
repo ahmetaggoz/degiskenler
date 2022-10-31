a53cbf9e98d45bb498a49a6a10ee8bed46ec45c3
Ahmet Aggoz <ahmetaggoz1388@gmail.com>
2022-10-31 22:13:39 +0300

    degiskenler tanıtıldı.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string name = "Ahmet";
+string surname = "Ağgöz";
+Console.WriteLine("Merhaba " + name + " " + surname);
+int yas = 27;
+decimal sayi = 3.30M;
+double sayi2 = 2.20;
+Console.WriteLine("Merhaba Benim adım " + name + " " + surname + " " + "Yaşım " + yas);
